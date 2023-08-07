25f1dbdcd36a89d0fad9ef7b49a228daeede2737
kunal raj shah <rajkunal7279@gmail.com>
2023-08-07 13:27:05 +0530

    use Context API to pass the props

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
+  // Listen context API like this
+  const ctxt=useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctxt.isLogedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxt.isLogedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxt.isLogedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctxt.onLogout}>Logout</button>
           </li>
         )}
       </ul>
